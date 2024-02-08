aa6f5028cb3ab5992af62efe5ea4056184575ef5
Manojblnr <104062249+Manojblnr@users.noreply.github.com>
2024-02-08 07:23:57 +0530

    delete App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,1 @@
 
-const  { callFakeAPI } = require( './App');
-const fetchMock = 'jest-fetch-mock';
-
-
-fetchMock.enableMocks();
-
-describe('API Call Count Test', () => {
-  beforeEach(() => {
-    fetchMock.resetMocks();
-  });
-
-  it('should make the correct number of API calls based on the number of winners', async () => {
-    const winners = ['X', 'O', 'X']; 
-    await callFakeAPI(winners);
-    expect(fetchMock).toHaveBeenCalledTimes(winners.length);
-  });
-});
